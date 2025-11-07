a47e116647f673972e9f3f5d89bc9f9f40fd6954
kmarara <mararakudakwashe3@gmail.com>
2025-11-07 19:35:44 +0200

    fix(dashboard): display table entries by passing page_obj context to template
    - Corrected context variable passed to emission_list template for pagination
    - Resolved issue causing empty dashboard table
    - Ensured alignment of view context and template usage for paginated queries

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ from .views import submit_emission
 urlpatterns = [
     #submit-emission/
     path('', views.submit_emission, name='submit_emission'),
-    path('submit-emission/success/', views.submit_emission_success, name='submit_emission_success')
+    path('submit-emission/success/', views.submit_emission_success, name='submit_emission_success'),
+    path('emissions/', views.emission_list, name='emission_list'), # New table view
 ]
 
 # new form is at http://127.0.0.1:8000/submit-emission/
--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import EmissionEntryForm
-from .models import EmissionEntry
-
+from .models import EmissionEntry, Supplier
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -22,7 +22,35 @@ def submit_emission_success(request):
 
 
 # View for login
-@login_required
+#@login_required
 def emission_list(request):
-    emissions = EmissionEntry.objects.all().order_by('-date_reported')
-    return render(request, 'core/emission_list.html', {'emissions': emissions})
+    # http://127.0.0.1:8000/emissions/
+    # Get filter values from GET request
+    supplier_id = request.GET.get('supplier')
+    verified = request.GET.get('verified')
+
+
+    entries = EmissionEntry.objects.all().order_by('-date_reported')
+
+    if supplier_id:
+        entries = entries.filter(supplier__id=supplier_id)
+
+    if verified == "true":
+        entries = entries.filter(verified=True)
+
+    elif verified == "false":
+        entries = entries.filter(verified=False)
+
+    # For filter dropdown
+    suppliers = Supplier.objects.all()
+
+    paginator = Paginator(entries, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
+    return render(request, 'core/emission_list.html',
+                  {
+                   'suppliers': suppliers,
+                   'page_obj': page_obj
+                   })
